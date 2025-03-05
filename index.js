da2c7eb25f6b4c2a14c96bd7b0b71df3ec75c1e3
isaias <isaias.gaona@zentro.com.mx>
2025-03-05 16:02:14 -0600

    avances

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// Conexión a la base de datos
+const connection = async () => {
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/mi_red_social');
+        console.log('Conexión a la base de datos establecida');
+    }catch(error){
+        throw new Error('Error al conectar a la base de datos');
+    }
+}
+
+module.exports = {
+    connection
+};
