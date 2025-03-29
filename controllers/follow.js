d3fd4cdafbbdb5941123351fab47d151cb736389
isaias <isaias.gaona@zentro.com.mx>
2025-03-28 18:47:56 -0600

    subir archivo qr sucursales

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,6 @@
+const Follow = require('../models/follow'); // Importamos el modelo de Follow
+const User = require('../models/user'); // Importamos el modelo de User
+
 // Acciones de prueba
 const pruebasFollow = (req,res) => {
     res.status(200).send({
@@ -5,6 +8,14 @@ const pruebasFollow = (req,res) => {
     })
 }
 
+const save = (req, res) => {
+    
+    // Recoger los datos de la petición
+    const params = req.body; // Recogemos el body de la petición
+
+}
+
 module.exports = {
-    pruebasFollow
+    pruebasFollow,
+    save
 }
--- routes/follow.js
+++ routes/follow.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const followController = require('../controllers/follow');
+const check = require('../middlewares/auth'); // Middleware de autenticación
 
 router.get('/prueba-follow', followController.pruebasFollow);
+router.post('/follow', check.auth ,followController.save);
 
 module.exports = router;
