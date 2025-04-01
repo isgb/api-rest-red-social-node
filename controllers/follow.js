c62432858a0db6ed962d2c4c5c475d882697edb0
isaias <isaias.gaona@zentro.com.mx>
2025-04-01 15:16:23 -0600

    avances

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,6 +1,9 @@
 const Follow = require('../models/follow'); // Importamos el modelo de Follow
 const User = require('../models/user'); // Importamos el modelo de User
 
+// Importar dependencias
+const mongoosePaginate = require('mongoose-pagination'); // Importamos la dependencia de paginación
+
 // Acciones de prueba
 const pruebasFollow = (req,res) => {
     res.status(200).send({
@@ -74,7 +77,46 @@ const unfollow =async (req, res)=>{
 // Accion de listado de usuarios que cualquier usuario esta siguiendo
 const following = async (req, res) => {
     // Sacar el id del usuario que sigue
-    const userId = req.user.id; // Sacamos el id del usuario que sigue desde el token
+    let userId = req.user.id; // Sacamos el id del usuario que sigue desde el token
+
+    // Comprobar si me llego el id por parametro de la url
+    if(req.params.id) {
+        userId = req.params.id; // Si me llega el id por parametro lo guardo en userId
+    }
+
+    // Comprobar si me llega la pagina, si no la pagina es 1
+    if(!req.params.page || req.params.page == null || req.params.page == undefined || isNaN(req.params.page)) {
+        var page = 1; // Si no me llega la pagina la pongo en 1
+    } else {
+        var page = req.params.page; // Si me llega la pagina la guardo en page
+    }
+
+    // Usuarios por pagina quiero mostrar
+    const itemsPerPage = 5; // Definimos cuantas entradas queremos mostrar por pagina
+
+    // Find a follow, porpular datos de los usuarios y paginar con mongoosePaginate
+    try {
+        const follows = await Follow.find({ user: userId })
+                            .populate({ path: 'followed', select: '-password -__v -role' })
+                            .paginate(page, itemsPerPage).exec();
+        if (!follows) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'No tienes usuarios seguidos'
+            });
+        }
+        return res.status(200).send({
+            status: 'success',
+            message: 'Usuarios seguidos',
+            follows
+        });
+    } catch (err) {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Error al listar los usuarios seguidos',
+            error: err.message
+        });
+    }
 
     // Comprobar si existe el usuario
     try {
